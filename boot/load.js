6b7ebf237781f0dbaa0ef9ecd0d7e125635a2033
giver of content <110511216+bloohacked@users.noreply.github.com>
2023-12-14 16:16:15 +1100

    Update load.js

--- boot/load.js
+++ boot/load.js
@@ -23,6 +23,7 @@ var scripts = [
   'program/minesweeper.js',
   'program/paint.js',
   '//xpstore.glitch.me/appstore.js',
+  '//xdstore.glitch.me/appstore.js',
   'boot/boot.js'
 ];
 var stylesheets = [
