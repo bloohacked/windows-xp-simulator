f47e94f674b24555f6c307edba6de7fe29885b62
giver of content <110511216+nathblade16@users.noreply.github.com>
2024-09-23 14:40:43 +1000

    Create bootModules.js
    yay bootModules now exist and you can now add your own boot scripts

---
+++ system/bootModules.js
@@ -0,0 +1,10 @@
+for (i=1;i<parseInt(localStorage.getItem("amntOfBootModules")) + 1;i++){
+    console.log(i);
+    eval(localStorage.getItem(i));
+}
+
+function newBootModule(code){
+    let slot = parseInt(localStorage.getItem("amntOfBootModules")) + 1;
+    localStorage.setItem(slot, code);
+    localStorage.setItem("amntOfBootModules", slot);
+}
